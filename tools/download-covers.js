d67445d2eae6cceb52268c0de2b2e0296ad8aac0
Alberto <martinez9alberto7@gmail.com>
2026-08-25 04:04:33 +0200

    Añadir Irène de Pierre Lemaitre y guía de novela negra francesa

--- tools/download-covers.js
+++ tools/download-covers.js
@@ -110,6 +110,7 @@ const OVERRIDES = {
   d9:  'title=Rich Dad Poor Dad&author=Robert Kiyosaki',
   r6:  'title=Divine Rivals&author=Rebecca Ross',
   r18: 'title=A Court of Silver Flames&author=Sarah J. Maas',
+  t29: 'isbn=9788466333122',
 };
 
 async function findCandidates(book) {
